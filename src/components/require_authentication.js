c46db1427112c8c35ee815dd2772fb7e6c8cc076
Rona ¯\_(ツ)_/¯ <deasnataly@gmail.com>
2018-04-24 08:29:32 +0700

    Handling edge cases: vid 45

--- src/components/require_authentication.js
+++ src/components/require_authentication.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 
 const container = (ComposedComponent) => {
+
   class Authentication extends Component {
+    componentWillMount(){
+      const {authenticated} = this.props;
+       if(!authenticated)
+         this.context.router.push('/');
+    }
+
+    componentWillUpdate(nextProps){
+      if(!nextProps.authenticated){
+        this.context.router.push('/');
+      }
+    }
+
     render() {
-      console.log(this.props.authenticated);
       return (
           <ComposedComponent {...this.props} />
       );
     }
   }
 
+  //wtf how the f this loc get the router object
+  //for this authenticate class
+  Authentication.contextTypes = {
+    router: React.PropTypes.object
+  };
+
+
   const mapStateToProps = (state) => ({
-    authenticated: state.authenticated
+    authenticated: state.authenticated,
   });
 
   return connect(mapStateToProps)(Authentication);
