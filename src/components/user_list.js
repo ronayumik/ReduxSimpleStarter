447165b4f1ad731470fb0d07b51a092b17161c98
Rona ¯\_(ツ)_/¯ <deasnataly@gmail.com>
2018-04-26 22:54:20 +0700

    vid 51: Rendering List of User

--- src/components/user_list.js
+++ src/components/user_list.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react';
+import * as actions  from '../actions';
+import { connect } from 'react-redux';
 
 class UserList extends Component {
-  renderUser(user){
-
+  componentWillMount() {
+    this.props.fetchUsers();
   }
+
+  renderUser(user) {
+    return (
+        <div className="card card-block">
+          <h4 className="card-title">
+            {user.name}
+          </h4>
+          <p className="card-text">
+            Dummy Company
+          </p>
+          <a href="" className="btn btn-primary">
+            Email
+          </a>
+        </div>
+    );
+  }
+
   render() {
     return (
-
+        <div>
+          {this.props.users.map(this.renderUser)}
+        </div>
     );
   }
 }
+
+const mapStateToProps = state => ({
+  users: state.users
+});
+
+export default connect(mapStateToProps, actions)(UserList);
